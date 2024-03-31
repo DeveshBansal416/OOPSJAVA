bb3c04b9d74225da9f689a576cbf83d88ce9b64e
DeveshBansal416 <74129608+DeveshBansal416@users.noreply.github.com>
2024-03-31 12:17:45 +0530

    Lecture1 completed

--- OOPSJAVA/src/Durgeshjava.java
+++ OOPSJAVA/src/Durgeshjava.java
@@ -1,9 +1,69 @@
+import java.util.*;
 
 public class Durgeshjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Durgesh Github");
+/*		System.out.println("Hello Durgesh Github");
+		
+		System.out.print("Hello World !!");
+		System.out.println("Hello World");//does not change the line
+		
+		int a=56;
+		System.out.print(a+"!");
+		a=20;
+		System.out.println(a);
+		
+		int b=50;
+		b+=20;
+		System.out.println(b);
+		
+		b++;
+		String name="John";
+		System.out.println(name);
+	*/	
+		
+	/*	Scanner scn=new Scanner(System.in);
+		String day=scn.nextLine();
+		
+		switch(day)
+		{
+		case "MON":
+			System.out.println("MONDAY");
+			break;
+		case "TUE":
+			System.out.println("TUESDAY");
+			break;
+		case "WED":
+			System.out.println("WEDNESDAY");
+			break;
+		default:
+			System.out.println("INVALID INPUT");
+		}
+	*/
+		
+		/*int nums[]= {12,13,15};
+		int s=0;
+		for(int x:nums) {
+			s+=x;
+		}
+		System.out.println(s);
+	*/
+		
+		for(int i=0;i<=100;i++) {
+			System.out.println(i);
+			
+			if(i==50) {
+				continue;
+			}
+			
+			if(i==50) {
+				break;
+			}
+			
+		}
+		System.out.println("end");
+	
+	
 	}
-
 }
